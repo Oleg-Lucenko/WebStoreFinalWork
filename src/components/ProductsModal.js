91fa200d258cdae3859da06884485f8c34f1daa1
Oleg-Lucenko <lucenko5577@gmail.com>
2023-06-12 01:57:56 +0300

    Final-project

--- src/components/ProductsModal.js
+++ src/components/ProductsModal.js
@@ -1,15 +1,20 @@
 import './ProductsModal.css'
 const ProductsModal = (props) => {
-console.log(props.children)
+
     return (
+
+    <div className="modal-registration-container">
+
+        <a href="#" className={props.active ? 'overlay active' : 'overlay'}></a>
 
         <div className={props.active ? "products_modal active" : 'products_modal'}>
             <div className="products_modalContent">
             <button className="products_modal_close_btn" onClick={() => props.setActive(false)}>X</button>
                 {props.children}
-            <button className="products_modal_buy_btn" onClick={() => props.setActive(false)}>Купити</button>
+            <button className="products_modal_buy_btn" onClick={() => {props.setActive(false); props.addToBasket(props.selectedItem)}}>Купити</button>
             </div>
         </div>
+    </div>
 
     )}
 export default ProductsModal;
